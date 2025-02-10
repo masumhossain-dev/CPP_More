215800802c619b104556f273b1942bcf0afba0b1
masumhossain-dev <masumhossain.dev@gmail.com>
2025-02-11 02:37:54 +0600

    write the code for passing array in a function paramiter. in this code, get array size and elements from user.

---
+++ arrayPassInFunction.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int displayArray(int arr[], int arrSize);
+int main() {
+    int arrSize, result;
+    cout << "How many numbers you want to sum?: ";
+    cin >> arrSize;
+    cout << "Enter that " << arrSize << " numbers: ";
+    int arr[arrSize];
+    for (int i = 0; i < arrSize; i++) {
+        cin >> arr[i];
+    }
+    result = displayArray(arr, arrSize);
+    cout << "Your result is: " << result << endl;
+    return 0;
+}
+
+int displayArray(int arr[], int arrSize) {
+    int sum = 0;
+    for (int i = 0; i<arrSize; i++) {
+        sum = sum + arr[i];
+    }
+    return sum;
+}

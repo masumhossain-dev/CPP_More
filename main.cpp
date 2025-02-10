efb9dcd268b5863ead1966b5fadbcae0ffeae2d6
masumhossain-dev <masumhossain.dev@gmail.com>
2025-02-10 17:38:45 +0600

    Guessing game created

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cstdlib>
+#include <cmath>
 using namespace std;
 
 int main() {
 
+while (1) {
+    int guessNumber, randomNumbr;
+    cout << "Enter your guess number: " << endl;
+    cin >> guessNumber;
 
+    guessNumber = 1 + rand() % 5;
+
+    if (randomNumbr == guessNumber) {
+        cout << "You Have Won!" << endl;
+    }
+    else {
+        cout << "You Have Lose!, Please try again" << endl;
+        cout << "The random number is: " << randomNumbr << endl;
+    }
+}
 
     return 0;
 }
